ba6b3001cc88201c1411fdbd14d8d4763e941d0c
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-03-04 16:07:29 +0100

    comments

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/re
 const key = "9FZM8WUKPYLBQQUKHUB4XG7C9"
 const contentType = "json"
 
+// element selection
 const locationInput = document.querySelector("#adressInput")
 const locationInputSubmit = document.querySelector("#submitAdressInput")
 const unitSelection = document.getElementsByName("unit")
+const currentWeatherEl = document.querySelector("#currentWeather")
+const forecastWeatherEl = document.querySelector("#forecastWeather")
 
-let userLocation = "Primasens"
+let userLocation = "Pirmasens"
 let unit = "metric"
 
+
+// fetch the data depending on the location
 async function getData() {
 	try {
+		//dynamically build the url for fetching
 		const apiURL = `${baseURL}${userLocation}?unitGroup=${unit}&key=${key}&contentType=${contentType}`
 		
 		const response = await fetch(apiURL)
@@ -22,20 +28,28 @@ async function getData() {
 	}
 }
 
+// helper function to change the location
 function setLocation(location) {
 	if(location === undefined || location === "") return
 	userLocation = location	
 }
 
+// eventlistener for submitting new location
 locationInputSubmit.addEventListener("click", (e) => {
 	e.preventDefault()
 	setLocation(locationInput.value)
 	getData()
 })
 
+// eventlistener for unit selection
 unitSelection.forEach(radioBtn => {
 	radioBtn.addEventListener("click", (e) => {
 		unit = e.target.value		
 	})
 })
+
 getData()
+
+function renderCurrentWeather() {
+	
+}
