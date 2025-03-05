b05c08ef4a45bfa95275845408e3b23519853c20
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-03-05 12:26:06 +0100

    html setup for proper design

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 const key = "9FZM8WUKPYLBQQUKHUB4XG7C9"
 const contentType = "json"
+const elements = "&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Chumidity%2Cprecip%2Cprecipprob%2Cprecipcover%2Cpreciptype%2Csnow%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon"
 
 // element selection
 const locationInput = document.querySelector("#adressInput")
@@ -8,6 +9,7 @@ const locationInputSubmit = document.querySelector("#submitAdressInput")
 const unitSelection = document.getElementsByName("unit")
 const currentWeatherEl = document.querySelector("#currentWeather")
 const forecastWeatherEl = document.querySelector("#forecastWeather")
+
 
 let userLocation = "Pirmasens"
 let unit = "metric"
@@ -17,12 +19,11 @@ let unit = "metric"
 async function getData() {
 	try {
 		//dynamically build the url for fetching
-		const apiURL = `${baseURL}${userLocation}?unitGroup=${unit}&key=${key}&contentType=${contentType}`
+		const apiURL = `${baseURL}${userLocation}?unitGroup=${unit}${elements}&key=${key}&contentType=${contentType}`
 		
 		const response = await fetch(apiURL)
 		const responsData = await response.json()
-		console.log(responsData);
-		
+
 	} catch (err) {
 		console.log(err);
 	}
@@ -47,9 +48,3 @@ unitSelection.forEach(radioBtn => {
 		unit = e.target.value		
 	})
 })
-
-getData()
-
-function renderCurrentWeather() {
-	
-}
